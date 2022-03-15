36c66c69c8df35e679a218ab409817935d3691eb
yabbal <abbal.youness@gmail.com>
2022-03-15 14:14:27 +0100

    re-setup project with angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Exercice2Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Exercice2Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
