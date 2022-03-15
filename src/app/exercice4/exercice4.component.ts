c2dd8242a61f7543319672e61b510cf7ddf25783
yabbal <abbal.youness@gmail.com>
2022-03-15 14:43:14 +0100

    add props texts + show them

--- src/app/exercice4/exercice4.component.ts
+++ src/app/exercice4/exercice4.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercice4',
   templateUrl: './exercice4.component.html',
-  styleUrls: ['./exercice4.component.css']
+  styleUrls: ['./exercice4.component.css'],
 })
 export class Exercice4Component implements OnInit {
+  public texts: string[] = ['Un', 'Deux', 'Trois'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
