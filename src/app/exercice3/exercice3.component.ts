43ddec749fc76d83e7002dc7a7481cde23fc6368
yabbal <abbal.youness@gmail.com>
2022-03-15 14:31:07 +0100

    add paragraph + statut component

--- src/app/exercice3/exercice3.component.ts
+++ src/app/exercice3/exercice3.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercice3',
   templateUrl: './exercice3.component.html',
-  styleUrls: ['./exercice3.component.css']
+  styleUrls: ['./exercice3.component.css'],
 })
 export class Exercice3Component implements OnInit {
+  public statut = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
