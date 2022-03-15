2679be78b968d58e8f736048b769468678c5ce38
yabbal <abbal.youness@gmail.com>
2022-03-15 14:27:46 +0100

    setup exo1 + binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice1Component } from './exercice1/exercice1.component';
 
 @NgModule({
-  declarations: [AppComponent, Exercice2Component],
+  declarations: [AppComponent, Exercice1Component, Exercice2Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
